3e3e7e7f04ac60e44677a4f81486092a27180645
banuka <banukaramanayaka76@gmail.com>
2024-04-21 23:01:42 +0530

    other:plane repeat texture

--- 3dComponents/Plane.tsx
+++ 3dComponents/Plane.tsx
@@ -1,8 +1,16 @@
+import { useTexture } from "@react-three/drei"
+import { RepeatWrapping } from 'three'
 const Plane = () => {
+    // const texture = useLoader(TextureLoader, './floorGrid.png')
+    const texture = useTexture('./floorGrid.png')
+    texture.wrapS = RepeatWrapping  // Set wrap mode for X-axis
+    texture.wrapT = RepeatWrapping  // Set wrap mode for Y-axis
+    texture.repeat.set(50, 50)  // Set the number of repeats
     return (
-        <mesh rotation-x={-Math.PI * 0.5} scale={10} position={[0, -1, 0]} >
+        <mesh rotation-x={-Math.PI * 0.5} scale={100} position={[0, -1, 0]} >
             <planeGeometry />
-            <meshStandardMaterial color={'green'} />
+            <meshStandardMaterial map={texture} />
+
         </mesh>
     )
 }
