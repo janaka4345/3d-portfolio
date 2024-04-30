33b17e46d34ce66a1dcba1da9019f7fa6fa3a4ba
banuka <banukaramanayaka76@gmail.com>
2024-04-30 22:27:30 +0530

    other: rapier physics added

--- 3dComponents/World.tsx
+++ 3dComponents/World.tsx
@@ -3,15 +3,18 @@ import { Canvas } from "@react-three/fiber"
 import Experience from "./Experience"
 import { OrbitControls } from "@react-three/drei"
 import { Perf } from 'r3f-perf'
+import { Physics } from "@react-three/rapier"
 
 const World = () => {
     return (
-        <Canvas style={{ width: '100svw', height: '100svh' }}>
-            <Perf position="top-left" />
-            <OrbitControls />
-            <directionalLight position={[2, 2, 2]} />
-            <ambientLight />
-            <Experience />
+        <Canvas shadows style={{ width: '100svw', height: '100svh' }}>
+            <Physics >
+                <Perf position="top-left" />
+                <OrbitControls />
+                <directionalLight intensity={2} castShadow position={[2, 2, 2]} />
+                <ambientLight />
+                <Experience />
+            </Physics>
         </Canvas>
     )
 }
