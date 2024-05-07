5e329145240d919e6feec4146200bdb4b27b978a
Janaka dev <janakchamantha12@gmail.com>
2024-05-07 19:06:22 +0530

    feat:html hero section overlay added

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import World from "@/3dComponents/World";
+import Hero from "@/components/custom/Hero";
 import StartAnimation from "@/components/custom/StartAnimation";
+
 import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="relative">
+    <main className="relative w-[100svw] h-[100dvh]">
       <StartAnimation />
-      {/* {false && <Suspense fallback={<h1>loading...</h1>}>
+      <Hero />
+      {true && <Suspense fallback={<h1>loading...</h1>}>
         <World />
-      </Suspense>} */}
+      </Suspense>}
     </main>
   );
 }
--- components/custom/StartAnimation.tsx
+++ components/custom/StartAnimation.tsx
@@ -3,15 +3,17 @@ import { LazyMotion, domAnimation, m } from "framer-motion"
 
 const StartAnimation = () => {
     return (
-        <LazyMotion features={domAnimation}>
-            <m.div
-                initial={{ opacity: 0, x: "-100%" }}
-                animate={{ opacity: 1, x: 0, transition: { duration: 2 } }}
-                className='absolute  inset-0 w-[100svw] h-[100svh] bg-red-500 flex justify-center items-center'
-            >
-                <h1 className='w-fit text-4xl mx-auto uppercase '>Janaka Kariyawasam</h1>
-            </m.div>
-        </LazyMotion>
+        <>
+
+            <LazyMotion features={domAnimation}>
+                <m.h1 initial={{ y: "0" }} animate={{ y: "-600px", transition: { duration: 2 } }} className='absolute top-0 left-0 right-0 bottom-0 m-auto  w-fit h-fit z-50 text-4xl uppercase '>Janaka Kariyawasam</m.h1>
+                <m.div
+                    initial={{ x: "0" }}
+                    animate={{ x: "-100%", transition: { duration: 3 } }}
+                    className='absolute z-40  inset-0 w-[100svw] h-[100svh] bg-red-500 flex justify-center items-center'
+                >
+                </m.div>
+            </LazyMotion></>
 
     )
 }
