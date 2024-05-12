0392fe922dd304ac11e18d7588254ff927849a4c
Janaka dev <janakchamantha12@gmail.com>
2024-05-12 22:55:45 +0530

    feat:character action store added

---
+++ store/characterActionStore.ts
@@ -0,0 +1,31 @@
+import { CameraControls } from "@react-three/drei"
+import { Ref } from "react"
+import { create } from "zustand"
+export type State = {
+    forward: boolean
+    back: boolean
+    left: boolean
+    right: boolean
+    jump: boolean
+}
+
+type Action = {
+    setAction: (action: State) => void
+}
+
+export const useCharacterAction = create<State & Action>((set) => ({
+    forward: false,
+    back: false,
+    left: false,
+    right: false,
+    jump: false,
+
+    setAction: (newAction) =>
+        set(() => ({
+            forward: newAction.forward,
+            back: newAction.back,
+            left: newAction.left,
+            right: newAction.right,
+            jump: newAction.jump,
+        })),
+}))
