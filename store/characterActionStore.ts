31c5ea02374f14cc2d36bfe27c124a583208cdd3
Janaka dev <janakchamantha12@gmail.com>
2024-05-13 08:10:43 +0530

    refactor:character action store refactor

--- store/characterActionStore.ts
+++ store/characterActionStore.ts
@@ -1,14 +1,13 @@
-import { CameraControls } from "@react-three/drei"
+import { CameraControls, useKeyboardControls } from "@react-three/drei"
 import { Ref } from "react"
 import { create } from "zustand"
 export type State = {
-    forward: boolean
-    back: boolean
-    left: boolean
-    right: boolean
-    jump: boolean
+    forward?: boolean
+    back?: boolean
+    left?: boolean
+    right?: boolean
+    jump?: boolean
 }
-
 type Action = {
     setAction: (action: State) => void
 }
@@ -20,12 +19,13 @@ export const useCharacterAction = create<State & Action>((set) => ({
     right: false,
     jump: false,
 
-    setAction: (newAction) =>
+    setAction: (newAction) => {
         set(() => ({
             forward: newAction.forward,
             back: newAction.back,
             left: newAction.left,
             right: newAction.right,
             jump: newAction.jump,
-        })),
+        }))
+    },
 }))
