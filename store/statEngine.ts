aa070b5184f51f9edea9b9e4f8d632db0e672b06
Janaka dev <janakchamantha12@gmail.com>
2024-05-14 21:27:24 +0530

    other:state engine updated

--- store/statEngine.ts
+++ store/statEngine.ts
@@ -1,20 +1,39 @@
 import { CameraControls } from "@react-three/drei"
 import { Ref } from "react"
 import { create } from "zustand"
+
 type State = {
     camera: Ref<CameraControls> | null
     page: string
+    state: "idle" | "walk" | "run" | "fly" | "flyIdle"
+    prevState: State["state"] | null
+    prevAction: string | null
+    currAction: string | null
 }
 
 type Action = {
     setCameraRef: (cameraRef: State["camera"]) => void
     setPage: (page: State["page"]) => void
+    setState: (newState: State["state"]) => void
+    setPrevState: (prevNewState: State["state"]) => void
+    setPrevAction: (action: State["prevAction"]) => void
+    setCurrAction: (action: State["currAction"]) => void
 }
 
 export const useStateEngine = create<State & Action>((set) => ({
     camera: null,
     page: "home",
+    state: "idle",
+    prevState: null,
+
+    prevAction: null,
+    currAction: null,
 
     setCameraRef: (cameraRef) => set(() => ({ camera: cameraRef })),
     setPage: (page) => set(() => ({ page: page })),
+    setState: (newState) => set(() => ({ state: newState })),
+    setPrevState: (prevNewState) => set(() => ({ prevState: prevNewState })),
+
+    setPrevAction: (action) => set(() => ({ prevAction: action })),
+    setCurrAction: (action) => set(() => ({ currAction: action })),
 }))
