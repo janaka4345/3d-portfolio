56306569b6b85890d3d5714704dfb53d9428ae8b
Janaka dev <janakchamantha12@gmail.com>
2024-05-16 23:24:01 +0530

    feat:message component and  dialog added

--- components/custom/Message.tsx
+++ components/custom/Message.tsx
@@ -1,16 +1,39 @@
-import { buttonVariants } from "../ui/button"
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog"
+import { Button } from "../ui/button"
+
+
+import MessageForm from "./MessageForm"
 
 const Message = () => {
     return (
-        <a
-            href="#"
-            className={buttonVariants({
-                variant: "cta",
-                size: "none",
-            })}
-        >
-            Leave a Message
-        </a>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button
+                    variant="cta"
+                    size="none"
+                >
+                    Leave a Message
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Edit profile</DialogTitle>
+                    <DialogDescription>
+                        Make changes to your profile here. Click save when you're done.
+                    </DialogDescription>
+                </DialogHeader>
+
+                <MessageForm />
+            </DialogContent>
+        </Dialog>
     )
 }
 export default Message
---
+++ components/custom/MessageForm.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import { z } from "zod"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+
+const formSchema = z.object({
+    name: z.string().min(2).max(50),
+    email: z.string().min(2).max(50),
+    message: z.string().min(2).max(100),
+})
+
+const MessageForm = () => {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            message: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+
+        console.log(values)
+    }
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Name:</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Your Name" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email:</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Your email address" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="message"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Message:</FormLabel>
+                            <FormControl>
+                                <textarea placeholder="Your message" rows={4} cols={50}  {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Send Message</Button>
+            </form>
+        </Form>
+    )
+}
+export default MessageForm
