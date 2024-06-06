f2772c96ea57c9620de8202f65e6a48d2c13d82e
Janaka dev <janakchamantha12@gmail.com>
2024-06-06 06:27:06 +0530

    other:nextjs image remote patterns added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ["three"],
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+                pathname: '/dqjfskfxu/**',
+            },
+        ],
+    },
 }
 
 export default nextConfig
