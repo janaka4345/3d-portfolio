e7de8d1eca0a67afdf1890028529c06305ec0cc4
Janaka dev <janakchamantha12@gmail.com>
2024-06-13 19:56:01 +0530

    fix:style issue of touch input component

--- components/custom/TouchInput.tsx
+++ components/custom/TouchInput.tsx
@@ -64,7 +64,7 @@ const TouchInput = () => {
                 className="pointer-events-auto h-10 w-10   col-start-2 col-end-3 row-start-1 row-end-2     bg-white"
             >
                 <svg className="w-full h-full text-gray-800 mx-auto  " aria-hidden="true" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="m5 15 7-7 7 7" />
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m5 15 7-7 7 7" />
                 </svg>
 
             </div>
@@ -94,7 +94,7 @@ const TouchInput = () => {
                 className="pointer-events-auto h-10 w-10 mx-auto    bg-white col-start-3 col-end-4 row-start-2 row-end-3"
             >
                 <svg className="w-full h-full text-gray-800 " aria-hidden="true" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="m9 5 7 7-7 7" />
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m9 5 7 7-7 7" />
                 </svg>
 
             </div>
