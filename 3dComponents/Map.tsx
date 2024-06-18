c74301992d9caa4011778469523c4ec62817dd90
Janaka dev <janakchamantha12@gmail.com>
2024-06-18 22:35:43 +0530

    other:map updates

--- 3dComponents/Map.tsx
+++ 3dComponents/Map.tsx
@@ -2,7 +2,7 @@ import { useAnimations, useGLTF } from "@react-three/drei"
 import { useEffect } from "react"
 
 const Map = () => {
-    const map = useGLTF("./skillCity4.glb")
+    const map = useGLTF("./skillCity4new3Final.glb")
     const animation = useAnimations(map.animations, map.scene)
     useEffect(() => {
         // console.log('map', animation);
