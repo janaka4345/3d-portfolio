c30bfbfa69da2fee02afbd75027c7abcf994e762
Janaka dev <janakchamantha12@gmail.com>
2024-06-20 12:53:57 +0530

    feat: nodemailer initialised

---
+++ lib/nodemailer.ts
@@ -0,0 +1,8 @@
+import nodemailer from "nodemailer"
+export const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASSWORD,
+    },
+})
