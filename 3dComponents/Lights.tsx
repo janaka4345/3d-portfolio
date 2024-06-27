4125fa1d4af5fbc777c1ff8d56b15407fbf85023
Janaka dev <janakchamantha12@gmail.com>
2024-06-27 20:06:36 +0530

    feat:custom lights added

--- 3dComponents/Lights.tsx
+++ 3dComponents/Lights.tsx
@@ -1,14 +1,19 @@
+import { useStateEngine } from "@/store/statEngine"
 import { Environment, Sky } from "@react-three/drei"
+import ProjectsLights from "./ProjectsLights"
 
 const Lights = () => {
+    const page = useStateEngine((state) => state.page)
+
     return (
         <>
-            <Sky sunPosition={[100, 20, 100]} />
+            {page === 'skills' && <Sky sunPosition={[100, 20, 100]} />}
+            {page === 'home' && <Environment preset="apartment" background />}
+            {/* {page === 'projects' && <ProjectsLights />} */}
+
             <ambientLight intensity={0.3} />
             <directionalLight intensity={2} position={[10, 20, 10]} />
             <directionalLight intensity={2} position={[-20, 20, -20]} />
-
-            {/* <Environment preset="apartment" background /> */}
         </>
     )
 }
