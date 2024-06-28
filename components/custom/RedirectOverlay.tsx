7da6e9142143615ec50fcc9703541fdbe768d69d
Janaka dev <janakchamantha12@gmail.com>
2024-06-28 17:21:07 +0530

    refactor : replaced button with link for optimization

--- components/custom/RedirectOverlay.tsx
+++ components/custom/RedirectOverlay.tsx
@@ -1,16 +1,11 @@
-import { Button } from '@/components/ui/button'
 import {
     Dialog,
     DialogContent
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
+import Link from 'next/link';
 
 export default function RedirectOverlay({ open, githubUrl, websiteUrl, handleClose }: { open: boolean, githubUrl: string, websiteUrl: string, handleClose: (type: string) => void }) {
 
-
-    const handleClick = () => {
-
-        window.open('https://google.com/', '_blank', 'noopener,noreferrer');
-    };
 
     return (
         <Dialog open={open} onOpenChange={() => { handleClose('out') }} >
@@ -18,8 +13,8 @@ export default function RedirectOverlay({ open, githubUrl, websiteUrl, handleClo
             <DialogContent className="sm:max-w-[425px] z-50" onClick={(e) => { e.stopPropagation() }} >
 
                 <div className='flex flex-col mt-4 gap-4'>
-                    <Button onClick={handleClick}>Github page</Button>
-                    <Button onClick={handleClick}>Live Website</Button>
+                    <Link target="_blank" href={githubUrl}>Github page</Link>
+                    <Link target="_blank" href={websiteUrl}>Live Website</Link>
                 </div>
             </DialogContent>
         </Dialog>
