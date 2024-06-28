92e848d628fe8be5e57288312fd8e3c9264c43ea
Janaka dev <janakchamantha12@gmail.com>
2024-06-28 17:21:57 +0530

    other:inline  svg optimized

--- components/custom/Socials.tsx
+++ components/custom/Socials.tsx
@@ -1,17 +1,15 @@
+import { cn } from "@/lib/utils"
 import Link from "next/link"
+import { buttonVariants } from "../ui/button"
 
 const Socials = () => {
     return (
         <div className="flex flex-col md:flex-row gap-4 w-fit mx-auto md:w-full md:mx-0">
 
-            <Link className="w-fit" href='https://www.linkedin.com/in/janaka-kariyawasam1256?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BSMPOtSkJRUOqhO1DT7aysg%3D%3D'> <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                <path fillRule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clipRule="evenodd" />
-                <path d="M7.2 8.809H4V19.5h3.2V8.809Z" />
-            </svg>
+            <Link target="_blank" className={cn(buttonVariants({ variant: 'default' }), "w-fit text-gray-200")} href='https://www.linkedin.com/in/janaka-kariyawasam1256?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BSMPOtSkJRUOqhO1DT7aysg%3D%3D'> <svg className="w-6 h-6 " fill="currentColor" aria-hidden="true" viewBox="0 0 24 24"><path d="M12.5 8.8v1.7a3.7 3.7 0 0 1 3.3-1.7c3.5 0 4.2 2.2 4.2 5v5.7h-3.2v-5c0-1.3-.2-2.8-2.1-2.8-1.9 0-2.2 1.3-2.2 2.6v5.2H9.3V8.8h3.2ZM7.2 6.1a1.6 1.6 0 0 1-1 1.5 1.6 1.6 0 0 1-1.7-.4 1.6 1.6 0 0 1 1.1-2.7 1.6 1.6 0 0 1 1.6 1.6ZM7.2 8.8H4v10.7h3.2V8.8Z" /></svg>
             </Link>
-            <Link className="w-fit" href='https://github.com/janaka4345'><svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                <path fillRule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clipRule="evenodd" />
-            </svg>
+            <Link target="_blank" className={cn(buttonVariants({ variant: 'default' }), "w-fit text-gray-200")} href='https://github.com/janaka4345'>
+                <svg className="w-6 h-6 " fill="currentColor" aria-hidden="true" viewBox="0 0 24 24"><path fillRule="evenodd" d="M12 2a9.8 9.8 0 0 0-6.5 2.4 10.3 10.3 0 0 0-3.4 6.2 10.5 10.5 0 0 0 1.3 7A10 10 0 0 0 8.8 22c.5 0 .7-.2.7-.5v-2c-2.8.7-3.4-1.1-3.4-1.1a2.7 2.7 0 0 0-1-1.5c-1-.7 0-.7 0-.7a2 2 0 0 1 1.5 1.1l.6.7a2 2 0 0 0 2.3.2c0-.6.3-1 .7-1.4-2.2-.3-4.6-1.2-4.6-5a4 4 0 0 1 1-2.8 3.8 3.8 0 0 1 .2-2.7s.8-.3 2.7 1a9.3 9.3 0 0 1 5 0c2-1.3 2.8-1 2.8-1 .3.8.4 1.8 0 2.7a4 4 0 0 1 1 2.7c0 4-2.3 4.8-4.5 5a2.5 2.5 0 0 1 .7 2v2.8c0 .3.2.6.7.5a10 10 0 0 0 5.4-4.4 10.5 10.5 0 0 0 1.3-7 10.3 10.3 0 0 0-3.4-6.2A9.8 9.8 0 0 0 12 2Z" clipRule="evenodd" /></svg>
             </Link>
         </div>
     )
