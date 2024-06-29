f91d5700e7c79b088ba4a7e42771f4f802de01ac
Janaka dev <janakchamantha12@gmail.com>
2024-06-29 15:50:42 +0530

    style: style changes to welcome screen

--- components/custom/Hero.tsx
+++ components/custom/Hero.tsx
@@ -28,16 +28,12 @@ const Hero = () => {
                         className=" pointer-events-none absolute  z-30   mx-auto mt-8 h-[100svh] w-[100svw] bg-transparent  px-8 py-8 lg:mt-16 lg:h-[60svh] lg:w-[60svw] lg:px-16  lg:py-16  "
                     >
                         <div className="mr-auto place-self-center ">
-                            <h1 className="mb-4 max-w-2xl text-center text-4xl font-extrabold leading-3 tracking-wider  md:text-left md:text-5xl xl:text-6xl">
-                                I bring
-                                <span className="bg-red-500"> ideas </span>
-                                to
-                                <span className="bg-green-500"> life </span>
-                                , from
-                                <span className="bg-blue-500"> DESIGN </span>
-                                to
-                                <span className="bg-yellow-500"> DATABASE </span>
-                                !
+                            <h1 className="mb-4 max-w-2xl text-center text-4xl font-extrabold leading-none tracking-tighter  md:text-left md:text-5xl xl:text-6xl">
+                                I bring IDEAS to LIFE
+                                <br />
+                                from
+                                <br />
+                                DESIGN to DATABASE!
                             </h1>
                             <p className="mb-6 max-w-[80dvw] text-center text-sm font-light text-gray-500 md:max-w-[60dvw] md:text-left md:text-lg lg:mb-8 lg:text-xl">
                                 I'm a full-stack web developer passionate about
