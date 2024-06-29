25dab356b881a1f0d9a997bd5efb9ebcaf132eb3
Janaka dev <janakchamantha12@gmail.com>
2024-06-29 15:48:56 +0530

    fix: docker host not re routing properly

--- 3dComponents/BillboardCarasoul.tsx
+++ 3dComponents/BillboardCarasoul.tsx
@@ -7,31 +7,34 @@ import { useCarouselStore } from "@/store/carasoulStore"
 import BillBox from "./BillBox"
 function BillboardCarasoul() {
     const carasoul = useCarouselStore((state) => state.carousel)
-    console.log(carasoul);
+
 
     const data = useMemo(() => {
-        console.log('memo ran');
 
         const data = [
             {
+                // githubUrl: 'http://165.232.179.119:3000/githubPhotosnap',
                 githubUrl: 'http://localhost:3000/githubPhotosnap',
                 githubRealUrl: 'https://github.com/janaka4345/photosnap-2',
                 websiteUrl: 'https://photosnap-pearl.vercel.app/'
 
             },
             {
+                // githubUrl: 'http://165.232.179.119:3000/githubBasicdesign',
                 githubUrl: 'http://localhost:3000/githubBasicdesign',
                 githubRealUrl: 'https://github.com/janaka4345/basicdesign',
                 websiteUrl: 'https://bruno-simon.com/html/'
 
             },
             {
+                // githubUrl: 'http://165.232.179.119:3000/githubBasicdesign',
                 githubUrl: 'http://localhost:3000/githubBasicdesign',
                 githubRealUrl: 'https://github.com/janaka4345/basicdesign',
                 websiteUrl: 'https://basicdesign-nine.vercel.app/'
 
             },
             {
+                // githubUrl: 'http://165.232.179.119:3000/githubPhotosnap',
                 githubUrl: 'http://localhost:3000/githubPhotosnap',
                 githubRealUrl: 'https://github.com/janaka4345/photosnap-2',
                 websiteUrl: 'https://photosnap-pearl.vercel.app/'
