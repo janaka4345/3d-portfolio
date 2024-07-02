87e7409af4b99fd058caaec3a7f3559b3a681f05
Janaka dev <janakchamantha12@gmail.com>
2024-07-02 13:25:36 +0530

    style:minor style changes to button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
                 ghost: "hover:bg-accent hover:text-accent-foreground",
                 link: "text-primary underline-offset-4 hover:underline",
                 customLink:
-                    "bg-primary-700 hover:bg-primary-800 focus:ring-primary-300  pointer-events-auto mr-1 lg:mr-3 inline-flex cursor-pointer items-center justify-center rounded-lg px-3  lg:px-5 py-3 text-center text-base font-medium text-white focus:ring-4",
+                    "bg-primary hover:bg-primary focus:ring-primary  pointer-events-auto mr-1 lg:mr-3 inline-flex cursor-pointer items-center justify-center rounded-lg px-3  lg:px-5 py-3 text-center text-base font-medium text-white focus:ring-4",
                 cta: "pointer-events-auto inline-flex cursor-pointer items-center justify-center rounded-lg border border-gray-300 px-3 lg:px-5 py-3 text-center text-base font-medium text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 ",
                 none: "",
             },
@@ -42,7 +42,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-        VariantProps<typeof buttonVariants> {
+    VariantProps<typeof buttonVariants> {
     asChild?: boolean
 }
 
