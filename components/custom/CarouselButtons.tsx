a24efee28436ae0918d9e30e70a314353a8f51e1
Janaka dev <janakchamantha12@gmail.com>
2024-08-04 22:47:14 +0530

    style: carasoul button style changes for clarity

--- components/custom/CarouselButtons.tsx
+++ components/custom/CarouselButtons.tsx
@@ -21,9 +21,9 @@ const CarouselButtons = () => {
 
     return (
         <>
-            <Button onClick={() => handleClick('dec')} className="touch-none   z-40 cursor-pointer text-4xl absolute w-fit h-fit left-6 top-1/2" >&lt;</Button>
+            <Button onClick={() => handleClick('dec')} className="touch-none  w-28 py-2  z-40 cursor-pointer text-4xl absolute   h-fit left-6 top-1/2" >&lt;<span className="ml-3 text-sm text-wrap w-10">Prev Project</span></Button>
 
-            <Button onClick={() => handleClick('inc')} className="touch-none  z-40 cursor-pointer text-4xl absolute w-fit h-fit right-6 top-1/2" >&gt;</Button>
+            <Button onClick={() => handleClick('inc')} className="touch-none w-28 py-2 z-40 cursor-pointer text-4xl absolute  h-fit right-6 top-1/2" ><span className="mr-3 text-sm text-wrap w-10">Next Project</span>&gt;</Button>
 
         </>
     )
