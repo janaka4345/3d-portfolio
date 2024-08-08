5ad78fd39063eb4a28b0e2609923a8cc551e351d
Janaka dev <janakchamantha12@gmail.com>
2024-08-08 09:53:01 +0530

    other:better ui animations added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-// 'use client'
+'use client'
 import World from "@/3dComponents/World"
 import Hero from "@/components/custom/Hero"
 import StartAnimation from "@/components/custom/StartAnimation"
@@ -6,21 +6,29 @@ import Loading from "@/components/skeletons/Loading"
 // import { useStateEngine } from "@/store/statEngine";
 // import { AnimatePresence } from "framer-motion";
 
-import { Suspense } from "react"
+import { Suspense, useEffect, useState } from "react"
 
 export default function Home() {
+    const [cue, setCue] = useState(false)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCue(true)
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, [])
 
     return (
-        <main className="relative h-[100dvh] w-[100svw] overflow-hidden">
+        <main className="relative h-[100dvh] w-[100svw] overflow-hidden bg-[url('http://localhost:3000/lebombo_placeholder.jpg')] bg-cover bg-blend-lighten">
             <StartAnimation />
-            <Suspense
+            {cue && <Suspense
                 fallback={
                     <Loading className='w-16 h-16 absolute left-1/2 top-1/2' />
                 }
             >
                 <Hero />
                 <World />
-            </Suspense>
+            </Suspense>}
         </main>
     )
 }
--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -1,18 +1,13 @@
 "use client"
-import {
-    Sheet,
-    SheetClose,
-    SheetContent,
-    SheetFooter,
-    SheetTrigger,
-} from "@/components/ui/sheet"
 import { cn } from "@/lib/utils"
 import { useStateEngine, type Page } from "@/store/statEngine"
 import { Button, buttonVariants } from "../ui/button"
 import Email from "./Email"
 import Message from "./Message"
-import Socials from "./Socials"
 import MobileNav from "./MobileNav"
+import Socials from "./Socials"
+import { LazyMotion, domAnimation, m } from "framer-motion"
+import { toast } from "sonner"
 
 export type MenuItem = {
     name: string
@@ -47,10 +42,13 @@ export const Navbar = () => {
             case "skills":
                 setState("walk")
                 setPrevState("idle")
+                toast.info('Use Keyboard to move', { duration: 2500, position: 'bottom-center', })
                 break
             case "projects":
                 setState("walk")
                 setPrevState("idle")
+                toast.info('Use Keyboard to move', { duration: 2500, position: 'bottom-center' })
+
                 break
 
             default:
@@ -59,48 +57,53 @@ export const Navbar = () => {
     }
 
     return (
-        <nav className="fixed  left-0 top-0 z-50 w-full bg-transparent">
-            <div className="mx-auto flex max-w-screen-2xl flex-wrap items-center justify-between p-4">
-                <Button
-                    onClick={() => handleClick("home")}
-                    className={cn(
-                        buttonVariants({ variant: "default" }),
-                        "flex items-center space-x-3 focus:left-0  touch-none"
-                    )}
-                >
-                    <span className="self-center whitespace-nowrap text-base font-semibold lg:text-2xl ">
-                        J
-                    </span>
-                </Button>
+        <LazyMotion features={domAnimation}>
+            <m.nav
+                initial={{ y: "-20dvh" }}
+                animate={{ y: "0", transition: { duration: 3, delay: 2 } }} className="fixed  left-0 top-0 z-50 w-full bg-transparent">
+                <div className="mx-auto flex max-w-screen-2xl flex-wrap items-center justify-between p-4">
+                    <Button
+                        onClick={() => handleClick("home")}
+                        className={cn(
+                            buttonVariants({ variant: "default" }),
+                            "flex items-center space-x-3 focus:left-0  touch-none"
+                        )}
+                    >
+                        <span className="self-center whitespace-nowrap text-base font-semibold lg:text-2xl ">
+                            J
+                        </span>
+                    </Button>
 
-                <div className=" hidden w-auto lg:block ">
-                    <ul className="mt-0  flex   flex-row items-center  space-x-4  rounded-lg  p-0 font-medium lg:space-x-8  ">
-                        {menuItems.map((item) => (
-                            <li key={item.triggerName}>
-                                <Button
-                                    variant="default"
-                                    onClick={() =>
-                                        handleClick(item.triggerName)
-                                    }
-                                    className="focus:left-0 touch-none cursor-pointer rounded border-0 p-0 px-3 py-2 text-sm lg:text-lg"
-                                >
-                                    {item.name}
-                                </Button>
-                            </li>
-                        ))}
+                    <div className=" hidden w-auto lg:block ">
+                        <ul className="mt-0  flex   flex-row items-center  space-x-4  rounded-lg  p-0 font-medium lg:space-x-8  ">
+                            {menuItems.map((item) => (
+                                <li key={item.triggerName}>
+                                    <Button
+                                        variant="default"
+                                        onClick={() =>
+                                            handleClick(item.triggerName)
+                                        }
+                                        className="focus:left-0 touch-none cursor-pointer rounded border-0 p-0 px-3 py-2 text-sm lg:text-lg"
+                                    >
+                                        {item.name}
+                                    </Button>
+                                </li>
+                            ))}
 
-                    </ul>
+                        </ul>
+                    </div>
+                    <div className="hidden w-auto lg:flex justify-center items-center">
+                        <li className="flex flex-row">
+                            <Email />
+                            <Message />
+                        </li>
+                        <Socials />
+                    </div>
+
+                    <MobileNav menuItems={menuItems} handleClick={handleClick} />
+
                 </div>
-                <div className="hidden w-auto lg:flex justify-center items-center">
-                    <li className="flex flex-row">
-                        <Email />
-                        <Message />
-                    </li>
-                    <Socials />
-                </div>
-
-                <MobileNav menuItems={menuItems} handleClick={handleClick} />
-            </div>
-        </nav>
+            </m.nav>
+        </LazyMotion>
     )
 }
